276f9be0d7f2bedbb09263347d545014d1714220
weizhangccnu <296448435@qq.com>
2017-08-19 17:47:06 +0800

    Add usleep() function

--- Hello_linux/Hello_linux.c
+++ Hello_linux/Hello_linux.c
@@ -2,7 +2,7 @@
 /***
  *
  *This is a C program for deliver parameter to main function
- *Main function received string
+ *Main function received string parameters
  *
  ***/
 /***************************************************************************/
@@ -10,13 +10,15 @@ int main(int argc, char **argv)
 {
     int i = 0;
     int parameter_one = 0;
+
     printf("%d\n",argc);
     parameter_one = atoi(argv[1]);          //convert string to int
     printf("%d\n",parameter_one);           //print integer number
 
-    for(i=0;i<parameter_one;i++)            //looping print number
+    for(i=0; i<parameter_one; i++)            //looping print number
     {
         printf("output number:%d\n",i);
+        usleep(100000);
     }
     printf("Hello Linux!\n");
     return 0;
