a762045720777dde5e52ffdfd49364e51fc34fc6
weizhangccnu <296448435@qq.com>
2017-10-15 19:48:22 +0800

    Add histogram2d test function

--- Hello_linux/hello_linux.c
+++ Hello_linux/hello_linux.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <hdf5.h>
+#include <gsl/gsl_rng.h>
 #include <gsl/gsl_histogram.h>
+#include <gsl/gsl_histogram2d.h>
 
 #define MAIN_DEGUB
 #define SQRT(x) ((x) * (x))
@@ -18,28 +20,62 @@ struct STUDENT(info)                  /* define a struct */
 };
 
 /***************************************************************************/
-/*This function is used to create a one dimension histogram
+/*This function is used to test the one dimension histogram library
  */
 int test_gsl_histogram(float a, float b, size_t n)
 {
+    FILE *fp;
     int i;
-    double dat[20] = {1, 15, 25, 23, 45, 48, 55, 56, 62, 65, 68, 70, 75, 80, 85, 90, 92, 98, 99};
-    //fp = fopen("output_file.dat", 'w');
-    //if (fp == NULL)
-    //{
-    //    printf("open file error!!\n");
-    //    exit(-1);
-    //}
+    double dat[20] = {32, 15, 25, 23, 45, 48, 55, 56, 62, 65, 68, 70, 75, 80, 85, 90, 92, 98, 99};
+    fp = fopen("histogram_output.dat", "w");            //file acessing method should be bracketed with ""
+    if (fp == NULL)
+    {
+        printf("open file error!!\n");
+        exit(-1);
+    }
     //fputc('a', fp);
-    //fclose(fp);
     gsl_histogram *h = gsl_histogram_alloc(n);          //allocates memory for histogram struct
     gsl_histogram_set_ranges_uniform(h, a, b);          //sets the ranges of the existing histogram
     for(i = 0; i < 20; i++) 
     {
         gsl_histogram_increment(h, dat[i]);             //adds one to the bin whose range
     }
-    gsl_histogram_fprintf(stdout, h, "%g", "%g");           //writes the ranges and bins of the histogram
+    printf("Sum of all bin values: %g\n", gsl_histogram_sum(h));
+    gsl_histogram_fprintf(fp, h, "%g", "%g");           //writes the ranges and bins of the histogram
     gsl_histogram_free(h);                              //frees the histogram pointer and all of the memory
+    fclose(fp);
+    return 0;
+}
+/***************************************************************************/
+/*This function is used to test the two dimension histogram library
+ */
+int test_gsl_histogram2d()
+{
+    FILE *fp;
+    const gsl_rng_type *T;                              //random number generator static information   
+    gsl_rng *r;                                         //define an instance of a generator 
+    fp = fopen("histogram2d_output.dat", "w");          //file acessing method should be bracketed with ""
+    if (fp == NULL)
+    {
+        printf("open file error!!\n");
+        exit(-1);
+    }
+    gsl_histogram2d  *h = gsl_histogram2d_alloc(10, 10);//allocates memory for histogram2d struct
+    gsl_histogram2d_set_ranges_uniform(h,               //sets the ranges of the existing histogram
+                                       0.0, 1.0,        
+                                       0.0, 1.0);
+    gsl_histogram2d_accumulate(h, 0.3, 0.3, 1);         //adding one (1.0) to the bin
+    gsl_histogram2d_accumulate(h, 0.8, 0.1, 5);
+    gsl_histogram2d_accumulate(h, 0.7, 0.9, 0.5);
+
+    gsl_rng_env_setup();                                //reads the environment variables
+    T = gsl_rng_default;                                
+    r = gsl_rng_alloc(T);
+
+    gsl_histogram2d_fprintf(fp, h, "%g", "%g");         //writes the ranges and bins of the histogram h line-by-line to the stream
+    gsl_histogram2d_free(h);                            //frees the histogram pointer and all of the memory
+    gsl_rng_free(r);                                    //frees random number pointer
+    fclose(fp);                                         //close file pointer
     return 0;
 }
 /***************************************************************************/
@@ -78,18 +114,10 @@ int sum_ab(int a, int b)
 #ifdef MAIN_DEGUB
 int main(int argc, char **argv)
 {
-    //FILE *fp;
     double a, b;
     size_t n;
     int i = 0;
     int parameter_one = 0;
-    //if((fp = fopen("output_file.dat", 'w')) == NULL)
-    //{
-    //    printf("open file error!!\n");
-    //    exit(-1);
-    //}
-    //fputc('a', fp);
-    //fclose(fp);
     if(argc != 5)
     {
         printf("Usage: gsl_histogram xmin xmax n\n"
@@ -113,7 +141,8 @@ int main(int argc, char **argv)
         printf("output number:%d\n",i);
         usleep(100000);                     //delay 100ms
     }
-    test_gsl_histogram(a, b, n);        //test gsl_histogram function
+    test_gsl_histogram(a, b, n);            //test gsl_histogram function
+    test_gsl_histogram2d();                 //test gsl_histogram2d function
     printf("Hello Linux!\n");
     return 0;
 }
