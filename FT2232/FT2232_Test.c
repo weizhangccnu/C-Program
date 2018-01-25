8c90f966bffed0ad86c58bf04749bae40884cb07
weizhangccnu <296448435@qq.com>
2018-01-25 15:17:57 +0800

    query driver version and library version

--- FT2232/FT2232_Test.c
+++ FT2232/FT2232_Test.c
@@ -8,12 +8,43 @@
  * under Linux operating system
  */
 /******************************************************************/
-
+// Get driver version
+void Get_driver_version(FT_HANDLE ftHandle)
+{
+    FT_STATUS ftStatus;
+    DWORD dwDriverVer;
+    ftStatus = FT_GetDriverVersion(ftHandle, &dwDriverVer);
+    if(ftStatus == FT_OK)
+    {
+        printf("Driver version = 0x%x\n", dwDriverVer); 
+    }
+    else
+    {
+        printf("error reading driver version\n");
+    }
+}
+/******************************************************************/
+// Get library version
+void Get_library_version()
+{
+    FT_STATUS ftStatus;
+    DWORD dwLibraryVer;
+    ftStatus = FT_GetLibraryVersion(&dwLibraryVer);
+    if(ftStatus == FT_OK)
+    {
+        printf("Library version = 0x%x\n", dwLibraryVer);
+    }
+    else
+    {
+        printf("error reading library version\n");
+    }
+}
 /******************************************************************/
 //main function
 int main()
 {
     int i = 0;
+    FT_HANDLE ftHandle;                 //define a ft2232 hanele
     FT_STATUS ftStatus;
     FT_HANDLE ftHandleTemp;
     DWORD numDevs;
@@ -23,6 +54,7 @@ int main()
     DWORD LocId;
     char SerialNumber[16];
     char Description[64];
+    //
     ftStatus = FT_CreateDeviceInfoList(&numDevs);
     if(ftStatus == FT_OK)
     { 
@@ -49,11 +81,29 @@ int main()
             printf(" ftHandle=0x%x\n", ftHandleTemp);
         }
     }
-    
-    for(i=0;i<10;i++)
+    //Opne ftdi device 
+    ftStatus = FT_OpenEx(SerialNumber, FT_OPEN_BY_SERIAL_NUMBER , &ftHandle);   //Open ftdi device
+    if(ftStatus == FT_OK)
+    {
+        printf("FT_Open succeed!\n");
+        Get_driver_version(ftHandle);
+        Get_library_version();
+    }
+    else
+    {
+        printf("FT_Open failed!\n");
+    }
+
+    //close handle
+    FT_Close(ftHandle);
+
+    //Test code 
+    for(i=0;i<3;i++)
     {
         printf("The %d OK!\n",i);
         usleep(8000);
     }
     printf("Running is over!\n");
+    return 0;
 }
+
